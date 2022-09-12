dafbe0e7e6b0a03e7e734e4fc204acae75931e8d
Stepan Shitikov <98213787+ThunderBoltCreator@users.noreply.github.com>
2022-09-12 22:19:11 +0300

    read2

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 const screens = document.querySelectorAll('.screen'),
-		start = document.querySelector('.start'),
-		timeList = document.querySelector('.time-list'),
-		timer = document.querySelector('#time'),
-		board = document.querySelector('#board'),
-		gameScore = document.querySelector('.score');
-		// reset = document.querySelector('.reset');
+	start = document.querySelector('.start'),
+	timeList = document.querySelector('.time-list'),
+	timer = document.querySelector('#time'),
+	board = document.querySelector('#board'),
+	gameScore = document.querySelector('.score'),
+	reset = document.querySelector('.reset');
 
 const colors = ['#c3a3a3', '#DDA0DD', '#018749', '#fd5c63', '#FEBE10'];
 
 let gameTime = 0;
 let score = 0;
 
+let game = false;
+let timerInterval;
 
 start.addEventListener('click', (e) => {
 	e.preventDefault();
@@ -44,32 +46,35 @@ board.addEventListener('click', e => {
 	}
 });
 
-// reset.addEventListener('click', () => {
-// 	screens[1].classList.remove('up');
-// 	score = 0;
-// 	timer.parentNode.classList.toggle('hide');
-// });
+reset.addEventListener('click', resetGame);
 
 function startGame() {
-	setInterval(decreaseTime, 1000);
+	game = true;
+	if (game === true) {
+		timerInterval = setInterval(() => {
+			if (gameTime > 0) {
+				decreaseTime();
+				console.log(gameTime);
+			}
+		}, 1000);
+	}
+	if (gameScore.classList.contains('hide')) {
+		gameScore.classList.remove('hide');
+	}
 	createRandomCircle();
 	setTime(gameTime);
-	// reset.classList.toggle('hide');
 }
 
 function decreaseTime() {
 	let current = --gameTime;
-	
+
 	if (current === 0) {
 		finishGame();
-	} 
-	// if (current > 0) {
-		if (current < 10) {
-			current = `0${current}`;
-		}
-		setTime(current);
-	// }
-
+	}
+	if (current < 10) {
+		current = `0${current}`;
+	}
+	setTime(current);
 }
 
 function setTime(value) {
@@ -78,16 +83,17 @@ function setTime(value) {
 
 function finishGame() {
 	timer.parentNode.classList.toggle('hide');
-	gameScore.classList.add('hide');
 	board.innerHTML = `<h1>Счет: <span class="primary">${score}</span></h1>`;
+	gameScore.classList.add('hide');
+	game = false;
 	// reset.classList.toggle('hide');
 }
 
 function createRandomCircle() {
 	const circle = document.createElement('div');
-	const size = getRandomNumber(10, 60);
-	const {width, height} = board.getBoundingClientRect();
-	
+	const size = getRandomNumber(20, 60);
+	const { width, height } = board.getBoundingClientRect();
+
 	const x = getRandomNumber(0, width - size);
 	const y = getRandomNumber(0, height - size);
 
@@ -110,11 +116,20 @@ function setColor(element) {
 	element.style.backgroundColor = color;
 }
 
-function getRandomColor () {
+function getRandomColor() {
 	const index = Math.floor(Math.random() * colors.length);
 	return colors[index];
 }
 
 function changeScore() {
 	gameScore.innerHTML = `Ваши очки: <span class="primary">${score}</span>`;
+}
+
+function resetGame() {
+	timer.parentNode.classList.toggle('hide');
+	score = 0;
+	gameTime = 0;
+	board.innerHTML = ``;
+	screens[1].classList.remove('up');
+	clearInterval(timerInterval);
 }
